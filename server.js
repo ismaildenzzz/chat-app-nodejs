b1afaf11d5d89e97a21910d35bdc1f257444588f
Hüseyin Kahraman <huseyinkahraman18@gmail.com>
2018-04-29 01:11:08 +0300

    Index template added.

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
+//var io = require('socket.io').listen(server);
+var path = require('path');
+
+
 
 server.listen(process.env.PORT || 3000);
 
+
+
 app.get('/',function(req,res){
-    res.sendFile(__dirname +'/index.html');
+    res.sendFile(__dirname +'/views/index.html');
 });
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
